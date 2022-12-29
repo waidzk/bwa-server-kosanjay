bd4cbb302436aceb23e4e67f28907d66bdaf5b41
waidzk <waidzk30@gmail.com>
2022-12-29 13:31:34 +0700

    full project server staycation

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const apiController = require("../controllers/apiController");
+const { upload } = require("../middleware/multer");
+
+
+router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
+router.post("/booking-page", upload, apiController.bookingPage);
+
+module.exports = router;

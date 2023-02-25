d0bdae0589b1da61184687cb9231d2828797d4c1
waidzk <waidzk30@gmail.com>
2023-02-25 15:03:34 +0700

    fix cors api in app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const flash = require('connect-flash');
-const cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const cors = require("cors");
 
 // import mongoose
-const mongoose = require('mongoose');
-const db_mongoose = "mongodb+srv://waidzk:kosanmin@cluster0.ag4km3f.mongodb.net/db_kosanjay?retryWrites=true&w=majority"
+const mongoose = require("mongoose");
+const db_mongoose =
+  "mongodb+srv://waidzk:kosanmin@cluster0.ag4km3f.mongodb.net/db_kosanjay?retryWrites=true&w=majority";
 mongoose.connect(db_mongoose, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,52 +19,63 @@ mongoose.connect(db_mongoose, {
   useFindAndModify: false,
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 //router
-var adminRouter = require('./routes/admin');
-var apiRouter = require('./routes/api');
+var adminRouter = require("./routes/admin");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 1000000 }
-}));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000000 },
+  })
+);
 app.use(flash());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/sb-admin-2",
+  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 //admin
-app.use('/admin', adminRouter);
-app.use('/api/v1/member', cors());
+app.use("/admin", adminRouter);
+app.use(
+  "/api/v1/member",
+  cors({
+    origin: "*",
+  }),
+  apiRouter
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
